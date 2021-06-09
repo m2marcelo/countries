349a145f368d6396fa713653c63fceabe8c5d106
Marcelo Morais <m2marcelo@gmail.com>
2021-06-09 16:02:09 +0200

    Adding the other rest API calls

--- app/src/main/java/eu/marcelomorais/countries/restApi/CountriesApi.kt
+++ app/src/main/java/eu/marcelomorais/countries/restApi/CountriesApi.kt
@@ -4,15 +4,27 @@ import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterF
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import eu.marcelomorais.countries.restApi.models.Country
+import eu.marcelomorais.countries.restApi.models.CountryDetails
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface CountriesService {
 
     @GET("all")
     fun getAll(): Call<List<Country>>
+
+    @GET("name")
+    fun getCountryByName(
+        @Query("name") name: String
+    ):Call<List<Country>>
+
+    @GET("name")
+    fun getCountryDetail(
+        @Query("name") name: String
+    ):Call<CountryDetails>
 
     companion object {
 
