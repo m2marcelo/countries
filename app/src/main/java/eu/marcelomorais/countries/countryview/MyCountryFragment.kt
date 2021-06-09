d31814be4e0b06809913a72445f3c10a38889fa8
Marcelo Morais <m2marcelo@gmail.com>
2021-06-09 16:13:26 +0200

    Adding permission handler to verify location permissions on my country fragment

--- app/src/main/java/eu/marcelomorais/countries/countryview/MyCountryFragment.kt
+++ app/src/main/java/eu/marcelomorais/countries/countryview/MyCountryFragment.kt
@@ -6,12 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import eu.marcelomorais.countries.R
 import eu.marcelomorais.countries.databinding.CountriesFragmentBinding
+import eu.marcelomorais.countries.utils.PermissionHandler
 
-class MyCountryFragment : Fragment() {
+class MyCountryFragment : Fragment(), PermissionHandler.PermissionListener  {
 
     private val viewModel: MyCountryViewModel by lazy {
         val application = requireNotNull(this.activity).application
@@ -20,6 +22,8 @@ class MyCountryFragment : Fragment() {
             .get(MyCountryViewModel::class.java)
 
     }
+
+    private val permissionUtil = PermissionHandler(this)
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
@@ -37,5 +41,23 @@ class MyCountryFragment : Fragment() {
 //        binding.viewModel = viewModel
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        permissionUtil.checkPermissions(this)
+    }
+
+    override fun onDestroyView() {
+        permissionUtil.unregister()
+        super.onDestroyView()
+    }
+
+    override fun onPermissionGranted() {
+//        All good, nothing to do here
+    }
+
+    override fun onPermissionDenied() {
+        Toast.makeText(requireContext(), getString(R.string.error_permission_denied), Toast.LENGTH_LONG).show()
     }
 }
