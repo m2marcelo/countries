f36856b0205c025accc28aa076161dfcf273ebe8
Marcelo Morais <m2marcelo@gmail.com>
2021-06-16 15:15:42 +0200

    Adding a sealed class to handle exceptions. Source:https://phauer.com/2019/sealed-classes-exceptions-kotlin/

---
+++ app/src/main/java/eu/marcelomorais/countries/repository/Outcome.kt
@@ -0,0 +1,6 @@
+package eu.marcelomorais.countries.repository
+
+sealed class Outcome<out T : Any> {
+    data class Success<out T : Any>(val value: T) : Outcome<T>()
+    data class Error(val message: String, val cause: Exception? = null) : Outcome<Nothing>()
+}
