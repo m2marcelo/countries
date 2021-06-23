f94c09ef80eda46f799230c967b5e940a7b0703a
Marcelo Morais <m2marcelo@gmail.com>
2021-06-23 12:04:26 +0200

    Added the recycler view

--- app/src/main/java/eu/marcelomorais/countries/adapters/CountriesAdapter.kt
+++ app/src/main/java/eu/marcelomorais/countries/adapters/CountriesAdapter.kt
@@ -1,27 +1,40 @@
 package eu.marcelomorais.countries.adapters
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import eu.marcelomorais.countries.database.CountriesDBModel
+import eu.marcelomorais.countries.databinding.CountryItemViewBinding
 
 
 class CountriesAdapter : ListAdapter <CountriesDBModel, CountriesAdapter.CountryViewHolder>(CountryDiffCallBack) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
-        TODO("Not yet implemented")
+        return CountryViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = getItem(position)
+        holder.binding(item)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    class CountryViewHolder(val dataBind: CountryItemViewBinding) : RecyclerView.ViewHolder(dataBind.root) {
 
+        companion object {
+            fun from(parent: ViewGroup) = CountryViewHolder(
+                CountryItemViewBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+        }
 
-    class CountryViewHolder : RecyclerView.ViewHolder() {
+        fun binding(item: CountriesDBModel) {
+            dataBind.countryItem = item
+            dataBind.executePendingBindings()
+        }
 
     }
 
