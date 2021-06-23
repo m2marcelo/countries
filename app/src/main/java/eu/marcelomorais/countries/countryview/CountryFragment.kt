cccb095e83869a7b71732a6720e6a2a066c5ddb0
Marcelo Morais <m2marcelo@gmail.com>
2021-06-23 12:04:00 +0200

    Changed the layout to be inflate, to check errors on recycler view

--- app/src/main/java/eu/marcelomorais/countries/countryview/CountryFragment.kt
+++ app/src/main/java/eu/marcelomorais/countries/countryview/CountryFragment.kt
@@ -35,7 +35,7 @@ class AllCountriesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_country, container, false)
+        return inflater.inflate(R.layout.countries_fragment, container, false)
     }
 
     companion object {
