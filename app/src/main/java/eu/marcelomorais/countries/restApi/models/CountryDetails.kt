fa3d97b8b2178ace7d8a0c937624658a80669ebe
Marcelo Morais <m2marcelo@gmail.com>
2021-06-30 00:25:01 +0200

    Removing unnecessary calls

--- app/src/main/java/eu/marcelomorais/countries/restApi/models/CountryDetails.kt
+++ app/src/main/java/eu/marcelomorais/countries/restApi/models/CountryDetails.kt
@@ -2,12 +2,10 @@ package eu.marcelomorais.countries.restApi.models
 
 import com.squareup.moshi.JsonClass
 
-@JsonClass(generateAdapter = true)
 data class Currency(
     val code: String,
     val name: String
 )
-@JsonClass(generateAdapter = true)
 data class Language(
     val iso639_1: String,
     val name: String,
@@ -16,9 +14,9 @@ data class Language(
 
 @JsonClass(generateAdapter = true)
 data class CountryDetails(
-    val id: Int,
-    val name: String,
-    val capital: String,
+    val id: Int?,
+    val name: String?,
+    val capital: String?,
     val callingCodes: List<String>,
     val region: String,
     val subregion: String,
