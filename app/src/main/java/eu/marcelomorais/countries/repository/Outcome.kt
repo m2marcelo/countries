2d72bd020c0e6a0de09377f7d4c317ba144d12fe
Marcelo Morais <m2marcelo@gmail.com>
2021-07-01 22:55:29 +0200

    Adding a loading state to help on a better view control. ref: https://medium.com/swlh/new-way-handle-state-event-in-android-with-sealed-classes-75ad63560574

--- app/src/main/java/eu/marcelomorais/countries/repository/Outcome.kt
+++ app/src/main/java/eu/marcelomorais/countries/repository/Outcome.kt
@@ -3,4 +3,5 @@ package eu.marcelomorais.countries.repository
 sealed class Outcome<out T> {
     data class Success<T>(val value: T) : Outcome<T>()
     data class Error<T>(val cause: Exception? = null) : Outcome<T>()
+    class Loading<T>() : Outcome<T>()
 }
