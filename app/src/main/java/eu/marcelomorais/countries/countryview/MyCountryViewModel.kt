524939ee5d4de5ff6c278898307bddd27b519f49
Marcelo Morais <m2marcelo@gmail.com>
2021-07-02 20:58:34 +0200

    Removed some logs

--- app/src/main/java/eu/marcelomorais/countries/countryview/MyCountryViewModel.kt
+++ app/src/main/java/eu/marcelomorais/countries/countryview/MyCountryViewModel.kt
@@ -24,11 +24,9 @@ class MyCountryViewModel (private val repository: CountriesRepository) : ViewMod
                     emptyList()
                 }
                 is Outcome.Success -> {
-                    Log.d("countriesList trans", "Success")
                     it.value
                 }
                 is Outcome.Loading -> {
-                    Log.d("myCountryLiveData", "Outcome.Loading")
                     emptyList()
                 }
             }
@@ -39,20 +37,15 @@ class MyCountryViewModel (private val repository: CountriesRepository) : ViewMod
     val loading: LiveData<Boolean> = _loading
 
     fun getCurrentCountry(country: CurrentCountry) {
-        Log.d("MyCountryViewModel", "getCurrentCountry")
         _currentCountry = country
         showProgressBar(true)
-        Log.d("MyCountryViewModel", "value = {$loading}")
-
         viewModelScope.launch {
             remoteGetCurrentData(country.countryName)
             showProgressBar(false)
-            Log.d("MyCountryViewModel after", "value = {$loading.value}")
         }
     }
 
     private suspend fun remoteGetCurrentData(countryName: String) {
-        Log.d("MyCountryViewModel", "remoteGetCurrentData = $countryName")
         repository.getMyCountry(countryName)
     }
 
@@ -70,8 +63,6 @@ class MyCountryViewModel (private val repository: CountriesRepository) : ViewMod
 
     fun onNavigateToMapFragment() {
         if( null != myCountryInfo.value?.get(0) ) {
-            val country: String = myCountryInfo.value!![0].countryName
-
             _myCountryDestinations.value = MyCountryFragmentDirections
                 .actionMyCountryFragmentToMapsFragment(_currentCountry)
         }
